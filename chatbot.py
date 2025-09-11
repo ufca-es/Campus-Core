e55e5f7e2d4964df8b23c1119680fb3ab420aad7
PgLandim <landim.gabriel@aluno.ufca.edu.br>
2025-09-11 12:47:49 -0300

    Corrigir a Task 10 de implementação do aprendizado automático e ajustar adicionar perguntas ao JSON

--- chatbot.py
+++ chatbot.py
@@ -1,24 +1,22 @@
 import random
-
 import json
-
 import os
+
 with open('json_chatbot.json', 'r', encoding='utf-8') as arq:
     dados = json.load(arq)
 
 # Arquivo para novas duvidas
 arquivo_novas_duvidas = 'novas_duvidas.json'
 
-#Se já existir um arquivo, ele faz o load do existente para não perder nenhuma dúvida anterior.
+#Arquivo para histórico - task 12 (Tendo que ser feita primeira)
+arquivo_historico = 'historico_chat.txt'
+
+#Se já existir um arquivo, ele faz o load do existente para não perder nenhuma dúvida anterior
 if os.path.exists(arquivo_novas_duvidas):
     with open(arquivo_novas_duvidas, 'r', encoding= 'utf-8') as arq:
         novas_duvidas = json.load(arq)
 else:
     novas_duvidas = []
-
-#Arquivo para histórico - task 12 (Tendo que ser feita primeira)
-
-arquivo_historico = 'historico_chat.txt'
 
 #Keywords para personalidades
 keywords_personalidade = {
@@ -27,13 +25,21 @@ keywords_personalidade = {
     "Rude": ["rude", "grosso", "sarcástico"]
 }
 
+#arquivo para aprendizado.
+arquivo_aprendizado = "aprendizado.txt"
+if os.path.exists(arquivo_aprendizado):
+    with open(arquivo_aprendizado, 'r', encoding='utf-8') as arq:
+        aprendizado = json.load(arq)
+else:
+    aprendizado = []
 
 def salvar_historico(pergunta,resposta):
     with open(arquivo_historico, 'a', encoding='utf-8') as arq:
         arq.write(f"Usuário:  {pergunta}\n")
         arq.write(f"Chatbot: {resposta}\n")
-        arq.write("="*20 + '\n')
+        arq.write("="*50 + '\n')
 
+#detecta as personalidades de acordo com: keywords_personalidade
 def detectar_personalidade(entrada, personalidade_atual="Formal"):
     entrada_lower = entrada.lower()
     for persona, keywords in keywords_personalidade.items():
@@ -44,7 +50,6 @@ def detectar_personalidade(entrada, personalidade_atual="Formal"):
     return personalidade_atual, entrada
 
 #Ler as 5 últimas interações
-
 def ler_ultimas_interacoes():
     if not os.path.exists(arquivo_historico):
         return []
@@ -65,32 +70,42 @@ def encontrar_respostas(duvida,personalidade):
                 if kw in duvida_lower:
                     respostas_possiveis.extend(item['resposta'])
                     break
+
+    for item in aprendizado:
+        if item['pergunta'].lower() in duvida_lower:
+            respostas_possiveis.append(item["resposta"])
+
     if respostas_possiveis:
         return random.choice(respostas_possiveis)
+    
     else:
-        if duvida not in novas_duvidas and duvida != "":
-            novas_duvidas.append(duvida)
-            with open(arquivo_novas_duvidas, 'w', encoding='utf-8') as arq:
-                json.dump(novas_duvidas,arq,ensure_ascii=False,indent=4)
-        return "Desculpa, ainda não sei responder a essa dúvida, mas guardei para analisar depois"
+        # Task 10: Reimplementada
+        print("Não sei responder a essa dúvida. Poderia me dizer uma possível resposta ? ")
+        sugestao_do_usuario = input("Diga sua sugestão: ")
+
+        novo_conhecimento = {"pergunta": duvida, "resposta": sugestao_do_usuario}
 
 
+        aprendizado.append(novo_conhecimento)
+        with open(arquivo_aprendizado, 'w', encoding='utf-8') as arq:
+            json.dump(aprendizado, arq, ensure_ascii=False, indent=4)
 
-#Modificar essa função de escolher pergunta
+        return "Obrigado! Aprendi algo novo. Na próxima vez já vou saber responder."
         
 def codigo_principal():
     personalidade_atual = "Formal"
-    print("Bem-vindo ao ChatBot UFCA! Você pode digitar qualquer pergunta.\n")
-    print("Para mudar a personalidade, use palavras como: engraçado, formal, rude.\nDigite sair para encerrar o programa")
 
     ultimas_interacoes = ler_ultimas_interacoes()
     
     if ultimas_interacoes:
-        print("Últimas interações.", "=" * 20)
+        print("Últimas interações.","\n","=" * 50)
         for interacao in ultimas_interacoes:
             for linha in interacao:
                 print(linha,end="")
-        print("="*20+"\n")
+        print("="*50+"\n")
+
+    print("Bem-vindo ao ChatBot UFCA! Você pode digitar qualquer pergunta.\n")
+    print("Para mudar a personalidade, use palavras como: engraçado, formal, rude.\nDigite sair para encerrar o programa")
 
     while True:
         duvida = input("Digite sua dúvida: ")
@@ -103,7 +118,6 @@ def codigo_principal():
         resposta = encontrar_respostas(duvida_limpa, personalidade_atual)
 
         print(f"({personalidade_atual}) Chatbot:", resposta)
-
 
         salvar_historico(duvida,resposta)
 
